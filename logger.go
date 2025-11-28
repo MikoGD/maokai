c068f4d6556aee9a2858cf7880139a176334ebf5
Mikael Escolin <mikaelescolin@gmail.com>
2025-11-28 11:26:05 +0000

    Add logger
    - Replace main.go with logger.go
    - Add function to create logger
    - Add struct for logger config and logger
    - Add error handling for logger

---
+++ logger.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"time"
+)
+
+type MissingLogDirectoryPathError struct{}
+type MissingLogNameError struct{}
+type DirectoryDoesNotExistError struct{ directoryPath string }
+
+func (e *MissingLogDirectoryPathError) Error() string {
+	return "Missing logDirecotoryPath in LoggerConfig"
+}
+
+func (e *MissingLogNameError) Error() string {
+	return "Missing logName in LoggerConfig"
+}
+
+func (e *DirectoryDoesNotExistError) Error() string {
+	return fmt.Sprintf("Directory %s does not exist", e.directoryPath)
+}
+
+type Logger interface {
+	CreateLog(body string) error
+}
+
+type LoggerConfig struct {
+	logDirectoryPath string
+	logName          string
+}
+
+type FileLogger struct {
+	file   *os.File
+	writer *bufio.Writer
+}
+
+func (fl *FileLogger) CreateLog(body string) error {
+	currDateTime := time.Now().UTC()
+
+	formattedLog := fmt.Sprintf("[%s]\n%s\n", currDateTime.Format(time.RFC3339), body)
+
+	if _, err := fl.writer.WriteString(formattedLog); err != nil {
+		return err
+	}
+
+	if err := fl.writer.Flush(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CreateLogger(config LoggerConfig) (*FileLogger, error) {
+	if config.logDirectoryPath == "" {
+		return nil, &MissingLogDirectoryPathError{}
+	}
+
+	_, err := os.Stat(config.logDirectoryPath)
+
+	if errors.Is(err, os.ErrNotExist) {
+		err := os.MkdirAll(config.logDirectoryPath, 0770)
+		log.Printf("Path %s does not existing, creating directory\n", config.logDirectoryPath)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if config.logName == "" {
+		return nil, &MissingLogNameError{}
+	}
+
+	logPath := path.Join(config.logDirectoryPath, config.logName)
+
+	file, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	writer := bufio.NewWriter(file)
+
+	logger := &FileLogger{file: file, writer: writer}
+
+	return logger, nil
+}
