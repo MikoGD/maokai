bb64f8d9064a4fc78da96808b677f14471abf7a5
Mikael Escolin <mikaelescolin@gmail.com>
2025-11-30 23:58:37 +0000

    Move errors into errors.go

---
+++ errors.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+type MissingLogDirectoryPathError struct{}
+type MissingLogNameError struct{}
+type DirectoryDoesNotExistError struct{ directoryPath string }
+
+func (e *MissingLogDirectoryPathError) Error() string {
+	return "Missing logDirecotoryPath in LoggerConfig"
+}
+
+func (e *MissingLogNameError) Error() string {
+	return "Missing logName in LoggerConfig"
+}
+
+func (e *DirectoryDoesNotExistError) Error() string {
+	return fmt.Sprintf("Directory %s does not exist", e.directoryPath)
+}
--- logger.go
+++ logger.go
@@ -10,22 +10,6 @@ import (
 	"time"
 )
 
-type MissingLogDirectoryPathError struct{}
-type MissingLogNameError struct{}
-type DirectoryDoesNotExistError struct{ directoryPath string }
-
-func (e *MissingLogDirectoryPathError) Error() string {
-	return "Missing logDirecotoryPath in LoggerConfig"
-}
-
-func (e *MissingLogNameError) Error() string {
-	return "Missing logName in LoggerConfig"
-}
-
-func (e *DirectoryDoesNotExistError) Error() string {
-	return fmt.Sprintf("Directory %s does not exist", e.directoryPath)
-}
-
 type Logger interface {
 	CreateLog(body string) error
 }
